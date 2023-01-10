0e83a49b3de088c23fd18470d164794ca7967d47
CoreySumma <CoreySumma@gmail.com>
2023-01-10 17:45:54 -0600

    create pSQL DB and home/about view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Define the home view
+# **REMINDER** Include an .html file extension
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
