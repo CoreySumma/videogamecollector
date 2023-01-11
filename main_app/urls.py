77a400aabc4b29b3be01fabe9fff4cab71488cb2
CoreySumma <CoreySumma@gmail.com>
2023-01-10 18:21:56 -0600

    render index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  # ALL GAMES
+  path('games/', views.games_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+games = [
+  {'title' : 'Demon Souls', 'description' : 'An action role-playing game where players take on the role of an adventurer, whose gender and appearance are customized at the beginning of the game, exploring the cursed land of Boletaria.'},
+  {'title' : 'Little Nightmares 2', 'description' : 'A suspense adventure game in which you play as Mono, a young boy trapped in a world that has been distorted by an evil transmission.'},
+  {'title' : 'World of Warcraft', 'description' : 'Set in the fictional world of Azeroth, WoW allows players to create avatar-style characters and explore a sprawling universe while interacting with other players'},
+]
+
 # Create your views here.
 # Define the home view
 # **REMINDER** Include an .html file extension
@@ -8,3 +14,8 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def games_index(request):
+  return render(request, 'games/index.html', {
+    'games' : games
+  })
