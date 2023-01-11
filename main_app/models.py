71df13ad03e2a04d41c13f7ce064d4a4d8ad8481
CoreySumma <CoreySumma@gmail.com>
2023-01-10 18:47:16 -0600

    create game model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+  title = models.CharField(max_length=150)
+  description = models.CharField(max_length=250)
+  progress = models.CharField(max_length=300)
+  # rating = models.IntegerChoices()
