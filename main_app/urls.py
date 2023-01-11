473ae2670588310bdbcc0f56253486726bd4dadc
CoreySumma <CoreySumma@gmail.com>
2023-01-10 19:44:43 -0600

    game details page render

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   # ALL GAMES
   path('games/', views.games_index, name='index'),
+  # GAME DETAIL
+  path('games/<int:game_id>/', views.games_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -21,3 +21,9 @@ def games_index(request):
   return render(request, 'games/index.html', {
     'games': games
   })
+
+def games_detail(request, game_id):
+  game = Game.objects.get(id=game_id)
+  return render(request, 'games/detail.html', {
+    'game': game
+  })
